649df50c03197d51f45aef36de9b34801dead284
Лукаш Павел Андреевич <lukash_pa@firon.org>
2022-07-12 08:35:32 +0300

    custom-educations modified, now generated. Atomized some svg-icons.

--- src/components/atoms/custom-educations.vue
+++ src/components/atoms/custom-educations.vue
@@ -1,73 +1,24 @@
 <template>
   <section class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto flex flex-wrap">
-      <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+
+      <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto" v-for="education in custom_educations"
+           :key="education.name">
         <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
           <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
         </div>
-        <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">1</div>
+        <div
+            class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+          {{ education.order }}
+        </div>
         <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-          <div class="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
-              <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-            </svg>
+          <div
+              class="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+            <svg-icon v-bind:type=education.svg_logo classes="w-12 h-12"></svg-icon>
           </div>
           <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-            <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Shooting Stars</h2>
-            <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
-          </div>
-        </div>
-      </div>
-      <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
-        <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
-          <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-        </div>
-        <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">2</div>
-        <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-          <div class="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-            </svg>
-          </div>
-          <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-            <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">The Catalyzer</h2>
-            <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
-          </div>
-        </div>
-      </div>
-      <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
-        <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
-          <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-        </div>
-        <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">3</div>
-        <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-          <div class="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
-              <circle cx="12" cy="5" r="3"></circle>
-              <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
-            </svg>
-          </div>
-          <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-            <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">The 400 Blows</h2>
-            <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
-          </div>
-        </div>
-      </div>
-      <div class="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
-        <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
-          <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-        </div>
-        <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">4</div>
-        <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-          <div class="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
-              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-              <circle cx="12" cy="7" r="4"></circle>
-            </svg>
-          </div>
-          <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-            <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Neptune</h2>
-            <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
+            <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">{{ education.name }}</h2>
+            <p class="leading-relaxed">{{ education.description }}</p>
           </div>
         </div>
       </div>
@@ -76,8 +27,54 @@
 </template>
 
 <script>
+import svgIcon from "@/components/utilities/svg-icon";
+// import request from "@/tools/request";
+
 export default {
-  name: "custom-educations"
+  name: "custom-educations",
+  components: {
+    svgIcon
+  },
+  data: () => ({
+    custom_education: {
+      name: '',
+      dates: '',
+      description: '',
+      svg_logo: '',
+      order: ''
+    },
+    custom_educations: []
+  }),
+  mounted() {
+    this.fetchMaterials()
+  },
+  methods: {
+    fetchMaterials() {
+
+      // request.get(`/api/admin/educations`).then((response) => {
+      //       this.custom_links = response.data.custom_links
+      //     }
+      // )
+
+      this.custom_educations = [
+        {
+          name: 'Shooting Stars',
+          dates: '2020-2022',
+          description: 'VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.',
+          svg_logo: 'shield',
+          order: 1
+        },
+        {
+          name: 'The Catalyzer',
+          dates: '2021-2022',
+          description: 'VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.',
+          svg_logo: 'heartbeat',
+          order: 2
+        }
+      ]
+
+    }
+  }
 }
 </script>
 
---
+++ src/components/utilities/svg-icon.vue
@@ -0,0 +1,53 @@
+<template>
+  <!--  Man logo svg-->
+  <svg v-if="type==='man'" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+       stroke-width="2"
+       v-bind:class="classes"
+       viewBox="0 0 24 24">
+    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+    <circle cx="12" cy="7" r="4"></circle>
+  </svg>
+  <!--  Anchor logo svg-->
+  <svg v-if="type==='anchor'" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+       stroke-width="2"
+       v-bind:class="classes"
+       viewBox="0 0 24 24">
+    <circle cx="12" cy="5" r="3"></circle>
+    <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
+  </svg>
+  <!--  Heartbeat logo svg-->
+  <svg v-if="type==='heartbeat'" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+       stroke-width="2"
+       v-bind:class="classes"
+       viewBox="0 0 24 24">
+    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+  </svg>
+  <!--  Shield logo svg-->
+  <svg v-if="type==='shield'" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+       stroke-width="2"
+       v-bind:class="classes"
+       viewBox="0 0 24 24">
+    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "svg-icon",
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    classes: {
+      type: String,
+      default: "w-12 h-12",
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
